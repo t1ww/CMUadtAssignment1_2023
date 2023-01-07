5f4959ec45f49adfb32aab2a155f386723e79462
t1ww <95153968+t1ww@users.noreply.github.com>
2023-01-07 20:16:12 +0700

    done most of the system
    wip : argument for selection

---
+++ t4/Student.java
@@ -0,0 +1,81 @@
+/*
+ * @author Narongchai Rongthong
+ * 652115013  dd/mm/yyyy
+ * written on 1/7/2023
+ * https://github.com/t1ww
+ */
+
+package t4;
+
+public class Student {
+    //initiailise variables zone
+    Integer index;
+    String SID;
+    String firstName;
+    String lastName;
+    //constructor
+    public Student(){
+        this.index = -1;
+        this.SID = "";
+        this.firstName = "";
+        this.lastName = "";
+    }
+    //overload
+    public Student(String SID, String firstName){
+        this.SID = SID;
+        this.firstName = firstName;
+    }
+    public Student(String SID, String firstName, String lastName){
+        this.SID = SID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public Student(int index, String SID, String firstName){
+        this.index = index;
+        this.SID = SID;
+        this.firstName = firstName;
+    }
+    public Student(int index, String SID, String firstName, String lastName){
+        this.index = index;
+        this.SID = SID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    //end overload
+    //set/
+    public void setIndex(int index){
+        this.index = index;
+    }
+    public void setSID(String SID){
+        this.SID =SID;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    //end set/
+    //get/
+    public int getIndex(){
+        return this.index;
+    }
+    public String getSID(){
+        return this.SID;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    //end get/
+    //toString template to show student's all available info
+    public String toString(){
+    String str = "string not found";
+        if(this.lastName == null){
+            str = String.format("[  | " + this.SID + " | " + this.firstName + " ]", str, null);
+        }else return "[ " + this.index + " | " + this.SID + " | " + this.firstName + " " + this.lastName + " ]";
+        return str;
+    }
+}
--- t4/t4adt.java
+++ t4/t4adt.java
@@ -3,12 +3,100 @@
  * 652115013  dd/mm/yyyy
  * written on 1/7/2023
  * https://github.com/t1ww
- */
-
+ */ 
 package t4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
 public class t4adt {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("\n//////////////new run////////////////\n");//buffer new run
+        Vector<Student> myStudents = new Vector<Student>();
+        File f = new File("student_list_65.csv");
+        Scanner scan = new Scanner(f);
+        //with every lines in the csv, seperate tokens into different datas and put into student object
+        //read csv line by line
+            while(scan.hasNextLine()){
+                String dataLine = scan.nextLine();
+                StringTokenizer token = new StringTokenizer(dataLine, ",");
+                int index = Integer.parseInt(token.nextToken());
+                String SID = token.nextToken();
+                String firstName = token.nextToken();
+                String lastName = "";
+                Student myStudent;
+                //put tokens into temporary object
+                if(token.hasMoreTokens()){ //if has last name
+                    lastName = token.nextToken();
+                    myStudent = new Student(index,SID,firstName,lastName);
+                }else{// dont have last name
+                    lastName = "";
+                    myStudent = new Student(index,SID,firstName);
+                }
+                //add temp object into dynamic array
+                myStudents.add(myStudent);
+            }
+        ///output // ex. [ 0 | 000000000 | Firstname Lastname ]
+            System.out.println("Total Students = " + myStudents.size());
+            //all students
+            showVectorLists(myStudents);
+            //myStudents >> [STUDENT1, STUDENT2(object id)]
+        //sort
+        System.out.print("After sorting by : ");
+        switch(args[0]){
+            case "-n" :// student number (sid)
+                System.out.println("student number (sid)");
+            break;
+            case "-f" :
+                System.out.println("First name");
+            break;
+            case "-l" :
+                System.out.println("last name");
+            break;
+            default:
+                System.out.println("no argument found, skipped sorting /");
+            break;
+        }
+            
+            sort(myStudents, "firstName");
+            //lists after sorted
+            showVectorLists(myStudents);
+            //program end
+            scan.close();//clean up
+            System.out.println("\n//////////////////////////////");
+            System.out.println("/////////End of THE PROGRAM");
+            System.out.println("//////////////////////////////");
+    }
+    //methods
+    private static void showVectorLists(Vector<Student> v){
+        for(Student std : v){
+            System.out.println(std);
+        }
+    }
+    public static void sort(Vector<Student> vList, final String field) {
+        Collections.sort(vList, new Comparator<Student>() {
+            /* (non-Javadoc)
+             * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+             */
+            @Override
+            public int compare(Student o1, Student o2) {
+                if(field.equals("index")) {
+                    return o1.index.compareTo(o2.index);
+                } else if(field.equals("SID")) {
+                    return o1.SID.compareTo(o2.SID);
+                } if(field.equals("firstName")) {
+                    return o1.firstName.compareTo(o2.firstName);
+                } if(field.equals("lastName")) {
+                    if(o1.lastName == null || o2.lastName == null)return -1;
+                    return o1.lastName.compareTo(o2.lastName);
+                }
+                return 0;
+            }           
+        });
     }
 }
