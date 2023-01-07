c39050e0d755cbf50046698a6ee9483dee76b8cf
t1ww <95153968+t1ww@users.noreply.github.com>
2023-01-07 16:47:17 +0700

    Create t4adt.java

---
+++ t4/t4adt.java
@@ -0,0 +1,14 @@
+/*
+ * @author Narongchai Rongthong
+ * 652115013  dd/mm/yyyy
+ * written on 1/7/2023
+ * https://github.com/t1ww
+ */
+
+package t4;
+
+public class t4adt {
+    public static void main(String[] args) {
+        
+    }
+}
